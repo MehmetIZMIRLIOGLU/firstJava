235d2a41646ef5a706d8353216832d9631cf8716
Mehmet İzmirlioğlu <mehmet@izmirlioglu.com>
2019-10-17 11:19:45 +0300

    5th Week - Lab

---
+++ src/week5_lab2.java
@@ -0,0 +1,12 @@
+public class week5_lab2 {
+    public static void main(String[] args) {
+        for(int i=1;i<50;i++)
+        {
+            for(int ii=1;ii<=i;ii++)
+            {
+                System.out.print(ii + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/week5_lab3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class week5_lab3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
+
+        boolean prime=true;
+
+        for (int i=2;i<num;i++)
+        {
+            if(num%i==0)
+                prime=false;
+        }
+
+        if(prime == true)
+            System.out.println(num + " is a prime number");
+        else
+            System.out.println(num + " is not prime number");
+    }
+}

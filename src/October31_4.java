bd2afeb65e9a742df2fbcf1f6f4282b1ac6ad2a3
Mehmet İzmirlioğlu <mehmet@izmirlioglu.com>
2019-10-31 11:47:21 +0300

    October31_4

---
+++ src/October31_4.java
@@ -0,0 +1,12 @@
+public class October31_4 {
+    public static void main(String[] args) {
+        // drawShape is public
+        October31_3.drawShape(50);
+
+        //bubble sort
+        //quick sort
+        //merge sort
+        //selection sort
+        //insertion sort
+    }
+}
